1d05668c6cf5b270c4ec1202b3fe9a40595e62be
perlancar <perlancar@gmail.com>
2014-10-14 10:44:03 +0000

    Import of PERLANCAR/Perinci-CmdLine-1.34 from CPAN.
    gitpan-cpan-distribution: Perinci-CmdLine
    gitpan-cpan-version:      1.34
    gitpan-cpan-path:         PERLANCAR/Perinci-CmdLine-1.34.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

--- lib/Perinci/CmdLine/Role/Help.pm
+++ lib/Perinci/CmdLine/Role/Help.pm
@@ -1,7 +1,7 @@
 package Perinci::CmdLine::Role::Help;
 
 our $DATE = '2014-10-14'; # DATE
-our $VERSION = '1.33'; # VERSION
+our $VERSION = '1.34'; # VERSION
 
 # split here just so it's more organized
 
@@ -640,7 +640,7 @@ Perinci::CmdLine::Role::Help - Help-related routines
 
 =head1 VERSION
 
-This document describes version 1.33 of Perinci::CmdLine::Role::Help (from Perl distribution Perinci-CmdLine), released on 2014-10-14.
+This document describes version 1.34 of Perinci::CmdLine::Role::Help (from Perl distribution Perinci-CmdLine), released on 2014-10-14.
 
 =for Pod::Coverage ^(.+)$
 
